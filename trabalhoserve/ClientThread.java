0a27ed23e277a11113db9c722c35c4a945f56c21
Joseviniciusrs <50296699+Joseviniciusrs@users.noreply.github.com>
2024-05-07 12:20:43 -0300

    Update ClientThread.java
    Adicionei mais perguntas e mudei algumas.

--- trabalhoserve/ClientThread.java
+++ trabalhoserve/ClientThread.java
@@ -24,7 +24,7 @@ class ClientThread implements Runnable {
             gab[2]="C";
             gab[3]="B";
             gab[4]="D";
-            while(questoes <= 5){
+            while(questoes <= 15){
             if ((inputLine = in.readLine()) != null) {
                 if (firstinput!=1) {
                     firstinput=1;
@@ -41,23 +41,64 @@ class ClientThread implements Runnable {
                     
                         break;
                     case 1:
-                    out.println("Quanto e 1-1 ;A)2 B)0 C)3 D)4");
+                    out.println("Qual o menor país do mundo ;A)Mônaco B)Vaticano C)San Marino D)Malta");
 
                         break;
                     case 2:
-                    out.println("Quanto e 2*1 ;A)10 B)0 C)2 D)4");
+                    out.println("Em qual local da Ásia o português é língua oficial ;A)Índia B)Moçambique C)Macau D)Filipinas");
 
                         break;
                     case 3:
-                    out.println("Quanto e 2*2 ;A)10 B)4 C)2 D)4");
+                    out.println("Quantos digitos tem o número PI ;A)Dezenas B)Infinitos C)Centenas D)Milhares");
 
                         break;
                     case 4:
-                    out.println("Quanto e 2/2 ;A)10 B)4 C)2 D)1");
+                    out.println("Quem pintou o teto da Capela Sistina ;A)Da Vinci B)Donatello C)Botticelli D)Michelangelo");
   
                         break;
                     case 5:
-                    out.println("Potuacao do cliente: " + pontuacao);
+                    out.println("Qual a capital do Canadá; A) Vancouver B) Toronto C) Montreal D) Ottawa");
+                        
+                         break;
+                    case 6:
+                    out.println("Por quantos anos Fidel Castro governou; A) 49 B) 32 C) 55 D) 46");
+                        
+                         break;
+                    case 7:
+                    out.println("Em que continente fica localizado o país Quirguistão; A) Ásia B) África C) Oceania D) Europa");
+                        
+                         break;
+                     case 8:
+                    out.println("Quem ganhou a copa do mundo de futebol em 94; A) Itália B) Alemanha C) Brasil D) Argentina");
+                        
+                         break;
+                     case 9:
+                    out.println("De qual franquia é o personagem Link; A) Resident Evil B) Zelda C) Sonic D) Mario");
+                        
+                         break;
+                     case 10:
+                    out.println("Qual dessas marcas de carro é alemã; A) AUDI B) FIAT C) CITROEN D) CHEVROLET");
+                        
+                         break;
+                     case 11:
+                    out.println("Qual a capital da República Tcheca; A) Londres B) Belgrado C) Bruxelas D) Praga");
+                        
+                         break;
+                     case 12:
+                    out.println("Em que ano acabou a segunda guerra mundial; A) 1918 B) 1945 C) 1994 D) 1967");
+                        
+                         break;
+
+                         case 13:
+                    out.println("Quem é o atual ditador da Coréia do Norte; A) Xi Jinping B) Kim Jong-un C) Kim Jong-il D) Qin Shihuang ");
+                        
+                         break;
+                         case 14:
+                    out.println("Quem criou a fórmula da gravidade; A) Einstein B) Tesla C) Oppenheimer D) Newton");
+                        
+                         break;
+                         case 15:
+                    out.println("Pontuação do cliente:" + pontuacao);
                          break;
                     default:
                         break;
